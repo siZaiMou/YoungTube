474a0e5325645468317c7a0f26f172c62bb950ec
libo <1054811808@qq.com>
2022-04-11 10:16:41 +0800

    添加heat实体，但点踩没了

---
+++ src/main/java/com/youngtube/demo/entity/VideoHeat.java
@@ -0,0 +1,14 @@
+package com.youngtube.demo.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class VideoHeat {
+    private int videoId;
+    private int categoryId;
+    private double dayHeat;
+    private double weekHeat;
+    private double yearHeat;
+    private double cityHeat;
+}
---
+++ src/main/java/com/youngtube/demo/mapper/VideoHeatMapper.java
@@ -0,0 +1,9 @@
+package com.youngtube.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface VideoHeatMapper {
+}
